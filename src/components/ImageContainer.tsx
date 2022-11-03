96c4810f320c676b81ca3e1499b654b76f6eaf1f
Rahime Yildiz <rahime1yildiz@gmail.com>
2022-11-03 21:08:37 +0100

    api fetched and word container added

---
+++ src/components/ImageContainer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container, Box } from "@mui/material";
+
+function ImageContainer() {
+  return (
+    <Container maxWidth="sm">
+      <Box sx={{ bgcolor: "#cfe8fc", height: "50vh" }} />
+    </Container>
+  );
+}
+
+export default ImageContainer;
---
+++ src/components/Question.tsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+
+export interface Response {
+  id: string;
+  word: string;
+  category: string;
+  numLetters: number;
+  numSyllables: number;
+  __v: number;
+}
+function Question() {
+  const [word, setWord] = useState<string>("");
+  const [hint, setHint] = useState<string>("");
+
+  useEffect(() => {
+    fetch("https://www.wordgamedb.com/api/v1/words/random").then((response) => {
+      response.json().then((data: Response) => {
+        setWord(data.word);
+        setHint(data.category);
+      });
+    });
+  }, []);
+
+  return (
+    <div>
+      <p style={{ textAlign: "center" }}>Guess What!!</p>
+      <p style={{ textAlign: "center" }}>Hint: {hint}</p>
+      <div style={{ textAlign: "center" }}>
+        {[...Array(word.length)].map((x, i) => (
+          <span key={i}>-</span>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Question;
