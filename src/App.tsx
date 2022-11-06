ef588a2765c92619e68515c9e0506fd383a9b446
Rahime Yildiz <rahime1yildiz@gmail.com>
2022-11-06 13:29:39 +0100

    keyboard added

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import ImageContainer from "./components/ImageContainer";
 import Question from "./components/Question";
+import KeyboardContainer from "./components/KeyboardContainer";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header title="Hangman" />
       <ImageContainer />
       <Question />
+      <KeyboardContainer />
     </>
   );
 }
